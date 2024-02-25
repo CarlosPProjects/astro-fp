734fa9e1b7258ded318b42e99973be6da8c5e208
CarlosPProjects <cg9822@gmail.com>
2024-02-25 22:00:18 +0100

    Header started and styled, icons from lucide.dev added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,37 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: 'class',
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-		container: {
-			center: true,
-			padding: '2rem',
-			screens: {
-				"2xl": "1440px",
-			}
-		}
-	},
-	plugins: [],
-}
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ["Nunito", ...defaultTheme.fontFamily.sans],
+        kanit: ["Kanit", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        ring: "#6D7D7D",
+        primary: {
+          DEFAULT: "#FFFFFF",
+          foreground: "#6D7D7D",
+        },
+        secondary: {
+          DEFAULT: "#131616",
+          foreground: "#212828",
+        },
+      },
+      letterSpacing: {
+        widest: "0.45em",
+      },
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        xl: "1204px",
+      },
+    },
+  },
+  plugins: [],
+};
